e9c9677db3e4a33a0f589399c7195edc2464444e
yudia <muhammadaprizal914@gmail.com>
2025-09-20 12:55:57 +0700

    initial commit

---
+++ src/main/kotlin/Boolean.kt
@@ -0,0 +1,8 @@
+package org.example
+
+
+fun main(args: Array<String>) {
+    var varBool : Boolean = true
+    if(varBool)
+        println("varBool bernilai " + varBool)
+}
---
+++ src/main/kotlin/String.kt
@@ -0,0 +1,9 @@
+package org.example
+
+fun main(args: Array<String>) {
+    val myName: String = "Muhammad Afrizal"
+    for(chr in myName){
+        print(chr)
+    }
+    print('\n')
+}
